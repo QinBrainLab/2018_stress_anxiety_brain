afc8f0c1c938a62765e50f74c70ed3474aa8a1b5
liuxiaomiao123 <42907892+liuxiaomiao123@users.noreply.github.com>
2020-07-10 21:12:26 +0800

    Add files via upload

---
+++ Network analysis/network_avg_control_0back.m
@@ -0,0 +1,63 @@
+% written by liangying,9/26/2019
+clear;
+clc;
+
+load('C:\Users\liuxiaomiao\Desktop\haiyang\RESULT\Network2\result\fc0_control.mat');
+
+fc0_control(isnan(fc0_control)) = 0;
+
+num = 32;    
+avg_FPN = zeros(1,num);     % 预分配内存提高效率
+avg_SN = zeros(1,num);
+avg_DMN = zeros(1,num);
+avg_FPN_DMN = zeros(1,num);
+avg_FPN_SN = zeros(1,num);
+avg_SN_DMN = zeros(1,num);
+
+
+for i=1:num
+    
+    r = fc0_control(:,:,i);
+    
+    FPN = r(3:4,3:4); 
+    s = (sum(FPN(:)))/2;
+    avg_FPN(i) = s/1;
+    
+    SN = r(5:6,5:6);  
+    s = (sum(SN(:)))/2 ;
+    avg_SN(i) = s/1;
+    
+    DMN = r(1:2,1:2);
+    s = (sum(DMN(:)))/2;
+    avg_DMN(i) = s/1;
+    
+    FPN_SN = r(3:4,5:6);
+    s = sum(FPN_SN(:));
+    avg_FPN_SN(i) = s/4;
+    
+    FPN_DMN = r(3:4,1:2);
+    s = sum(FPN_DMN(:));
+    avg_FPN_DMN(i) = s/4;
+    
+    SN_DMN = r(5:6,1:2);
+    s = sum(SN_DMN(:));
+    avg_SN_DMN(i) = s/4;
+    
+end
+
+avg_FPN = avg_FPN'
+avg_SN = avg_SN'
+avg_DMN = avg_DMN'
+avg_FPN_SN = avg_FPN_SN'
+avg_FPN_DMN = avg_FPN_DMN'
+avg_SN_DMN = avg_SN_DMN'
+all = [avg_FPN,avg_SN,avg_DMN ,avg_FPN_SN,avg_FPN_DMN,avg_SN_DMN]
+
+    save('C:\Users\liuxiaomiao\Desktop\haiyang\RESULT\Network2\control\result\control_0back\avg_FPN.mat', 'avg_FPN'); 
+    save('C:\Users\liuxiaomiao\Desktop\haiyang\RESULT\Network2\control\result\control_0back\avg_SN.mat', 'avg_SN');
+    save('C:\Users\liuxiaomiao\Desktop\haiyang\RESULT\Network2\control\result\control_0back\avg_DMN.mat', 'avg_DMN');
+    save('C:\Users\liuxiaomiao\Desktop\haiyang\RESULT\Network2\control\result\control_0back\avg_FPN_SN.mat', 'avg_FPN_SN');
+    save('C:\Users\liuxiaomiao\Desktop\haiyang\RESULT\Network2\control\result\control_0back\avg_FPN_DMN.mat', 'avg_FPN_DMN');
+    save('C:\Users\liuxiaomiao\Desktop\haiyang\RESULT\Network2\control\result\control_0back\avg_SN_DMN.mat', 'avg_SN_DMN');
+    save('C:\Users\liuxiaomiao\Desktop\haiyang\RESULT\Network2\control\result\control_0back\all.mat','all');
+
